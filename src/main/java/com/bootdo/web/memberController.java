5004117e90a81e2ab66085126a36770c955e9a69
wenyahui <18749563260@139.com>
2018-08-16 18:08:30 +0800

    开始做会员1

---
+++ src/main/java/com/bootdo/web/memberController.java
@@ -0,0 +1,40 @@
+/**
+ * 会员中心controller
+ */
+package com.bootdo.web;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bootdo.common.controller.BaseController;
+import com.bootdo.common.utils.MD5Utils;
+import com.bootdo.common.utils.PageUtils;
+import com.bootdo.common.utils.Query;
+import com.bootdo.common.utils.R;
+import com.bootdo.utils.CodeKit;
+import com.bootdo.web.service.ArticleService;
+import com.bootdo.web.service.ColumnService;
+
+@Controller
+@RequestMapping("/member")
+public class memberController extends BaseController {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+}
